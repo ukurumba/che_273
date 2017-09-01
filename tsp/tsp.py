56d4d76ff10565d59c4399434fadbaaa5fdab261
ukurumba <ukurumba@u.rochester.edu>
2017-09-01 18:40:19 -0400

    added all functions. no test cases

--- tsp/tsp.py
+++ tsp/tsp.py
@@ -1,4 +1,5 @@
 import random
+import math
 class tsp:
 
 
@@ -33,7 +34,8 @@ class tsp:
         Returns: List of shortest path.'''
 
         self.path = self.generate_random_path()
-        for i in range(num_its):
+        self.cost = calc_cost()
+        for k in range(num_its):
             try:
                 self.path = self.modify_path()
             except Exception as error:
@@ -65,17 +67,76 @@ class tsp:
             new_cost = new_cost + self.dist_mat[node2_neighbors['left']][node1] + self.dist_mat[node1][node2_neighbors['right']]
 
 
-        # Need to implement for when node1/node2 is 0 or last val. 
+        elif node1 == 0 and (node2 != self.path[-1]):
+            node1_neighbor = self.path[node1_index+1]
+            node2_neighbors = {'left' : self.path[node2_index-1], 'right': self.path[node2_index+1]}
+            new_cost = self.cost - self.dist_mat[node1][node1_neighbor]
+            new_cost = new_cost - self.dist_mat[node2_neighbors['left']][node2] - self.dist_mat[node2][node2_neighbors['right']]
+            new_cost = new_cost + self.dist_mat[node2][node1_neighbor]
+            new_cost = new_cost + self.dist_mat[node2_neighbors['left']][node1] + self.dist_mat[node1][node2_neighbors['right']]
+        elif node2 = 0 and (node1 != self.path[-1]):
+            node2_neighbor = self.path[node1_index+1] 
+            node1_neighbors = {'left' : self.path[node1_index-1], 'right': self.path[node1_index+1]}
+            new_cost = self.cost - self.dist_mat[node1_neighbors['left']][node1] - self.dist_mat[node1][node1_neighbors['right']] 
+            new_cost = new_cost - self.dist_mat[node2][node2_neighbor] 
+            new_cost = new_cost + self.dist_mat[node1_neighbors['left']][node2] + self.dist_mat[node2][node1_neighbors['right']]
+            new_cost = new_cost + self.dist_mat[node1][node2_neighbor]
+        elif node1 != 0 and node2 == self.path[-1]:         
+            node1_neighbors = {'left' : self.path[node1_index-1], 'right': self.path[node1_index+1]}
+            node2_neighbor = self.path[node2_index-1]
+            new_cost = self.cost - self.dist_mat[node1_neighbors['left']][node1] - self.dist_mat[node1][node1_neighbors['right']]  
+            new_cost = new_cost - self.dist_mat[node2_neighbor][node2] 
+            new_cost = new_cost + self.dist_mat[node1_neighbors['left']][node2] + self.dist_mat[node2][node1_neighbors['right']]
+            new_cost = new_cost + self.dist_mat[node2_neighbor][node1]
+        elif node2 != 0 and node1 == self.path[-1]:            
+            node2_neighbors = {'left' : self.path[node2_index-1], 'right': self.path[node2_index+1]}
+            new_cost = self.cost - self.dist_mat[node2_neighbors['left']][node2] - self.dist_mat[node2][node2_neighbors['right']]
+            new_cost = new_cost - self.dist_mat[node1_neighbor][node1] 
+            new_cost = new_cost + self.dist_mat[node2_neighbors['left']][node1] + self.dist_mat[node1][node2_neighbors['right']]
+            new_cost = new_cost + self.dist_mat[node1_neighbor][node2]
+        elif node1 == 0 and node2 == self.path[-1]:
+            node1_neighbor = self.path[node1_index+1]
+            node2_neighbor = self.path[node2_index-1]
+            new_cost = self.cost - self.dist_mat[node1][node1_neighbor] - self.dist_mat[node2_neighbor][node2]
+            new_cost = new_cost + self.dist_mat[node2][node1_neighbor] + self.dist_mat[node2_neighbor][node1]
+        elif node2 == 0 and node1 == self.path[-1]:
+            node1_neighbor = self.path[node1_index-1]
+            node2_neighbor = self.path[node2_index+1]
+            new_cost = self.cost - self.dist_mat[node2][node2_neighbor] - self.dist_mat[node1_neighbor][node1]
+            new_cost = new_cost + self.dist_mat[node1][node2_neighbor] + self.dist_mat[node1_neighbor][node2]
         if self.cost - new_cost > 0: #if new path better than old path
             self.path[node1_index] = node2
             self.path[node2_index] = node1
             self.cost = new_cost
         else:
-            prob_of_switch = self.calc_prob_of_switch(self.cost - new_cost)
+            T = self.calc_temp(k)
+            prob_of_switch = self.calc_prob_of_switch(self.cost - new_cost,T)
             if random.uniform(0,1) <= prob_of_switch: #randomly get number to see whether to switch or not
                 self.path[node1_index] = node2
                 self.path[node2_index] = node1
                 self.cost = new_cost
+
+    def calc_cost(self,path=[]):
+        if not path:
+            path = list(self.path)
+        cost = 0
+        for i,node in zip(range(len(path)),path):
+            if node != path[-1]: #if not last node 
+                cost += self.dist_mat[node][i+1]
+        return cost
+        
+
+    def calc_prob_of_switch(self,delta,T):
+        #calclates the probability you should switch to the more expensive path
+        return math.exp(-1*delta / T)
+    def calc_temp(self,k):
+        return (init_temp + beta * k)
+
+
+    
+
+            
+
 
     
             
