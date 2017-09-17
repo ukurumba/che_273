f619384ec9fed985471a4cfff57bc0b225096db2
ukurumba <ukurumba@u.rochester.edu>
2017-09-17 15:36:09 -0400

    cleans up code and writes unit test for modify path

--- tsp/tsp/tests.py
+++ tsp/tsp/tests.py
@@ -2,7 +2,8 @@ import unittest
 from tsp import tsp 
 
 class TestTSP(unittest.TestCase):
- 
+ 	
+
 	def setUp(self):
 		pass
  
@@ -14,7 +15,69 @@ class TestTSP(unittest.TestCase):
 		with self.assertRaises(TypeError):
 			tsp({'hi':12,'no':12},23.523,1) #tests distance matrix 
 		with self.assertRaises(IndexError):
-			tsp([12.2,31,2,3],1,24.3) #tests for list of lists
+			tsp([12.2,31,2,3],1,24.3) #tests for list of 
+
+	def testRandomPath(self):
+		tsp1 = tsp([[12,13,14],[12,121,2],[1,2,3]],12,13)
+		path = tsp1.generate_random_path()
+		self.assertTrue(len(path) == 4 and path[0] == path[-1])
+
+	def test_calc_cost(self):
+		tsp1 = tsp1 = tsp([[12,13,14],[12,121,2],[1,2,3]],12,13)
+		path = [0,1,2,0]
+		self.assertEqual(tsp1.calc_cost(path=path),16)
+
+	def test_choose_nodes(self):
+		tspA = tsp([[12,13,14],[12,121,2],[1,2,3]],12,13)
+		tspA.path = tspA.generate_random_path()
+		node1,node2 = tspA.choose_nodes()
+		truth = (node1==0 or node1 == 1 or node1 == 2) and (node2 == 0 or node2 == 1 or node2 == 2) and (node1 != node2)
+		self.assertTrue(truth)
+
+	def test_modify_path_first_node(self):
+
+		tsp1 = tsp([[1,2,3,4],[5,6,7,8],[9,10,11,12],[200,14,15,16]],12,13)
+		init_cost = 2 + 7 + 12 + 200
+		tsp1.path = [0,1,2,3,0]
+		nodes = (0,1)
+		tsp1.cost = tsp1.calc_cost()
+
+		tsp1.modify_path(nodes)
+		new_cost = 5 + 3 + 12 + 14
+		self.assertEqual(tsp1.cost, new_cost)
+		self.assertEqual(tsp1.path,[1,0,2,3,1])
+
+
+		tsp1 = tsp([[1,2,3,4],[5,6,7,8],[9,10,11,12],[200,14,15,16]],12,13)
+		init_cost = 2 + 7 + 12 + 200
+		tsp1.path = [0,1,2,3,0]
+		nodes = (1,0)
+		tsp1.cost = tsp1.calc_cost()
+
+		tsp1.modify_path(nodes)
+		new_cost = 5 + 3 + 12 + 14
+		self.assertEqual(tsp1.cost, new_cost)
+		self.assertEqual(tsp1.path,[1,0,2,3,1])
+
+	def test_modify_path_interior_nodes(self):
+		tsp1 = tsp([[1,2,3,4],[5,6,300,8],[9,10,11,12],[13,14,15,16]],12,13)
+		init_cost = 2 + 300 + 12 + 13
+		tsp1.path = [0,1,2,3,0]
+		nodes = (1,2)
+		tsp1.cost = tsp1.calc_cost()
+		self.assertEqual(init_cost,tsp1.cost)
+
+		tsp1.modify_path(nodes)
+		new_cost = 3 + 10 + 8 + 13
+		self.assertEqual(tsp1.cost, new_cost)
+		self.assertEqual(tsp1.path,[0,2,1,3,0])
+
+
+
+		
+
+
+
 
 if __name__ == '__main__':
 	unittest.main()
--- tsp/tsp/tsp.py
+++ tsp/tsp/tsp.py
@@ -19,11 +19,11 @@ class tsp:
             raise TypeError("Beta must be an integer or float.")
         if type(distance_matrix) != list:
             raise TypeError("Distance matrix must be a list of lists.")
-        elif len(distance_matrix) != 2:
+        elif not isinstance(distance_matrix[0],list):
             raise IndexError("Distance matrix must be two dimensional.")
         else:
             self.dist_mat = distance_matrix
-            self.num_nodes = len(self.adj_mat[0])
+            self.num_nodes = len(self.dist_mat[0])
             self.init_temp = init_temp
             self.beta = beta
             
@@ -42,87 +42,89 @@ class tsp:
                 raise error
         return self.path
 
-    def gemerate_random_path(self):
-        available_nodes = [i in range(self.num_nodes)]
+    def generate_random_path(self):
+        available_nodes = [i for i in range(self.num_nodes)]
         path = []
         while available_nodes:
             next_node = random.choice(available_nodes)
             path.append(next_node)
             available_nodes.remove(next_node)
+        path.append(path[0])
         return path
 
-    def modify_path(self):
-        #randomly chooses node to modify. updates cost and decides whether or not to keep new path
+    def choose_nodes(self):
+        #chooses one node, removes it from the available choices, then chooses the next node
         node1 = random.choice(self.path)
-        node2 = random.choice(list(self.path).remove(node1))
+        temp_path = list(self.path)
+        if node1 == temp_path[0]:
+            temp_path = temp_path[1:-1]
+        else:
+            temp_path.remove(node1)
+        node2 = random.choice(temp_path)
+        return (node1,node2)
+
+    def update_path(self,node1,node2):
 
         node1_index = self.path.index(node1)
         node2_index = self.path.index(node2)
-        if node1 != 0 and node1 != self.path[-1] and node2 != 0 and node2 != self.path[-1]:
-            node1_neighbors = {'left' : self.path[node1_index-1], 'right': self.path[node1_index+1]}
-            node2_neighbors = {'left' : self.path[node2_index-1], 'right': self.path[node2_index+1]}
-            new_cost = self.cost - self.dist_mat[node1_neighbors['left']][node1] - self.dist_mat[node1][node1_neighbors['right']] 
-            new_cost = new_cost - self.dist_mat[node2_neighbors['left']][node2] - self.dist_mat[node2][node2_neighbors['right']]
-            new_cost = new_cost + self.dist_mat[node1_neighbors['left']][node2] + self.dist_mat[node2][node1_neighbors['right']]
-            new_cost = new_cost + self.dist_mat[node2_neighbors['left']][node1] + self.dist_mat[node1][node2_neighbors['right']]
 
-
-        elif node1 == 0 and (node2 != self.path[-1]):
-            node1_neighbor = self.path[node1_index+1]
-            node2_neighbors = {'left' : self.path[node2_index-1], 'right': self.path[node2_index+1]}
-            new_cost = self.cost - self.dist_mat[node1][node1_neighbor]
-            new_cost = new_cost - self.dist_mat[node2_neighbors['left']][node2] - self.dist_mat[node2][node2_neighbors['right']]
-            new_cost = new_cost + self.dist_mat[node2][node1_neighbor]
-            new_cost = new_cost + self.dist_mat[node2_neighbors['left']][node1] + self.dist_mat[node1][node2_neighbors['right']]
-        elif node2 == 0 and (node1 != self.path[-1]):
-            node2_neighbor = self.path[node1_index+1] 
-            node1_neighbors = {'left' : self.path[node1_index-1], 'right': self.path[node1_index+1]}
-            new_cost = self.cost - self.dist_mat[node1_neighbors['left']][node1] - self.dist_mat[node1][node1_neighbors['right']] 
-            new_cost = new_cost - self.dist_mat[node2][node2_neighbor] 
-            new_cost = new_cost + self.dist_mat[node1_neighbors['left']][node2] + self.dist_mat[node2][node1_neighbors['right']]
-            new_cost = new_cost + self.dist_mat[node1][node2_neighbor]
-        elif node1 != 0 and node2 == self.path[-1]:         
-            node1_neighbors = {'left' : self.path[node1_index-1], 'right': self.path[node1_index+1]}
-            node2_neighbor = self.path[node2_index-1]
-            new_cost = self.cost - self.dist_mat[node1_neighbors['left']][node1] - self.dist_mat[node1][node1_neighbors['right']]  
-            new_cost = new_cost - self.dist_mat[node2_neighbor][node2] 
-            new_cost = new_cost + self.dist_mat[node1_neighbors['left']][node2] + self.dist_mat[node2][node1_neighbors['right']]
-            new_cost = new_cost + self.dist_mat[node2_neighbor][node1]
-        elif node2 != 0 and node1 == self.path[-1]:            
-            node2_neighbors = {'left' : self.path[node2_index-1], 'right': self.path[node2_index+1]}
-            new_cost = self.cost - self.dist_mat[node2_neighbors['left']][node2] - self.dist_mat[node2][node2_neighbors['right']]
-            new_cost = new_cost - self.dist_mat[node1_neighbor][node1] 
-            new_cost = new_cost + self.dist_mat[node2_neighbors['left']][node1] + self.dist_mat[node1][node2_neighbors['right']]
-            new_cost = new_cost + self.dist_mat[node1_neighbor][node2]
-        elif node1 == 0 and node2 == self.path[-1]:
-            node1_neighbor = self.path[node1_index+1]
-            node2_neighbor = self.path[node2_index-1]
-            new_cost = self.cost - self.dist_mat[node1][node1_neighbor] - self.dist_mat[node2_neighbor][node2]
-            new_cost = new_cost + self.dist_mat[node2][node1_neighbor] + self.dist_mat[node2_neighbor][node1]
-        elif node2 == 0 and node1 == self.path[-1]:
-            node1_neighbor = self.path[node1_index-1]
-            node2_neighbor = self.path[node2_index+1]
-            new_cost = self.cost - self.dist_mat[node2][node2_neighbor] - self.dist_mat[node1_neighbor][node1]
-            new_cost = new_cost + self.dist_mat[node1][node2_neighbor] + self.dist_mat[node1_neighbor][node2]
-        if self.cost - new_cost > 0: #if new path better than old path
+        if node1_index != 0 and node2_index != 0:
             self.path[node1_index] = node2
             self.path[node2_index] = node1
+
+        elif node1_index == 0:
+            self.path[0] = node2
+            self.path[-1] = node2
+            self.path[node2_index] = node1
+        elif node2_index == 0:
+            self.path[0] = node1
+            self.path[-1] = node1
+            self.path[node1_index] = node2
+
+
+    def modify_path(self,nodes):
+        node1,node2 = nodes
+        #randomly chooses node to modify. updates cost and decides whether or not to keep new path
+        node1_index = self.path.index(node1)
+        node2_index = self.path.index(node2)
+        temp_path = list(self.path)
+
+        if node1_index != 0 and node2_index != 0:
+            
+            temp_path[node1_index] = node2
+            temp_path[node2_index] = node1
+            new_cost = self.calc_cost(path=temp_path)
+
+        elif node1_index == 0:
+            
+            temp_path[0] = node2
+            temp_path[-1] = node2
+            temp_path[node2_index] = node1
+            new_cost = self.calc_cost(path=temp_path)
+
+        elif node2_index == 0:
+            temp_path[0] = node1
+            temp_path[-1] = node1
+            temp_path[node1_index] = node2
+            new_cost = self.calc_cost(path=temp_path)
+
+        if self.cost - new_cost > 0: #if new path better than old path
+            self.update_path(node1,node2)
             self.cost = new_cost
+
         else:
             T = self.calc_temp(k)
             prob_of_switch = self.calc_prob_of_switch(self.cost - new_cost,T)
             if random.uniform(0,1) <= prob_of_switch: #randomly get number to see whether to switch or not
-                self.path[node1_index] = node2
-                self.path[node2_index] = node1
+                self.update_path(node1,node2)
                 self.cost = new_cost
 
     def calc_cost(self,path=[]):
         if not path:
             path = list(self.path)
         cost = 0
-        for i,node in zip(range(len(path)),path):
-            if node != path[-1]: #if not last node 
-                cost += self.dist_mat[node][i+1]
+        for i in range(len(path)-1):
+            cost += self.dist_mat[path[i]][path[i+1]]
         return cost
         
 
@@ -130,7 +132,7 @@ class tsp:
         #calclates the probability you should switch to the more expensive path
         return math.exp(-1*delta / T)
     def calc_temp(self,k):
-        return (init_temp + beta * k)
+        return (self.init_temp + self.beta * k)
 
 
     
